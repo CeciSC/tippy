ea5e740f09cdc825c509917100c834b0b316774a
CeciSC <ceciliachen233@gmail.com>
2020-09-26 23:00:29 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Cecilia Chen on 9/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
